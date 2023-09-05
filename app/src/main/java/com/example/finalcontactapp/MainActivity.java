4640b3dfc47f2d36d1c507f34afa383605a5db5a
kajoljituri <jiturikajol@gmail.com>
2023-09-05 18:45:19 +0530

    Added Textview

--- app/src/main/java/com/example/finalcontactapp/MainActivity.java
+++ app/src/main/java/com/example/finalcontactapp/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.finalcontactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView abc;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        abc = (TextView) findViewById(R.id.abc);
     }
 }
