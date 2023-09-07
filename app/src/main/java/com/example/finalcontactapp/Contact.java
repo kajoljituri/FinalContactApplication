a0c58aae1519b4ce0acfc2ff8a7ac3df7107bb71
kajoljituri <jiturikajol@gmail.com>
2023-09-07 12:02:21 +0530

    contact Class created

---
+++ app/src/main/java/com/example/finalcontactapp/Contact.java
@@ -0,0 +1,51 @@
+package com.example.finalcontactapp;
+
+public class Contact {
+    String name,nickname,circle;
+    int phoneNumber;
+
+    public Contact(){
+
+    }
+
+    public Contact(String name, String nickname, String circle, int phoneNumber) {
+        this.name = name;
+        this.nickname = nickname;
+        this.circle = circle;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getnickname() {
+        return nickname;
+    }
+
+    public void setnickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getCircle() {
+        return circle;
+    }
+
+    public void setCircle(String circle) {
+        this.circle = circle;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
