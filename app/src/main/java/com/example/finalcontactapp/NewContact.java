2f4518d14263cf4567dbf6497e2a0c5f3bc834a0
kajoljituri <jiturikajol@gmail.com>
2023-09-09 16:29:11 +0530

    NewContact xml

--- app/src/main/java/com/example/finalcontactapp/NewContact.java
+++ app/src/main/java/com/example/finalcontactapp/NewContact.java
@@ -2,13 +2,50 @@ package com.example.finalcontactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class NewContact extends AppCompatActivity {
+    EditText name,phone,circle,nickname;
+    ImageButton backbtn;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
+        name =(EditText) findViewById(R.id.entername);
+        phone =(EditText) findViewById(R.id.enterphone);
+        nickname=(EditText) findViewById(R.id.enternick);
+        backbtn=(ImageButton) findViewById(R.id.backbtn);
+
+
+        spinner =  (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this,R.array.circle_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String data=adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(),data,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
